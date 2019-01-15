88d18aafefa14909e2f450ab6774c037289c79b8
Adam Quinn <aqauthor@gmail.com>
2019-01-14 20:19:33 -0500

    Received Message Processing
    The wixel will now receive incoming bytes from the radio and push them into a
    four-element Rx buffer. It will proceed to use timers to output the data in the
    Rx buffer while simultaneously handling Transmit tasks.

--- tappy_glove.c
+++ tappy_glove.c
@@ -9,9 +9,17 @@ http://www.pololu.com/docs/0J46
 #include <wixel.h>
 #include <usb.h>
 #include <usb_com.h>
+#include <radio_com.h>
+#include <radio_link.h>
 #include <stdio.h>
 
 #define TX_BUFF_LEN 100
+#define RX_BUFF_LEN 4
+
+#define DOT_TIME 250
+#define DASH_TIME 500
+#define GAP_TIME 250
+#define SPACE_TIME 1000
 
 typedef enum { false = 0, true = 1} bool;
 typedef enum MorseSymbol{INVALID = 0, DOT = 1, DASH = 2, SPACE = 3} morse_t;
@@ -19,6 +27,30 @@ typedef enum MorseSymbol{INVALID = 0, DOT = 1, DASH = 2, SPACE = 3} morse_t;
 morse_t txMessage[TX_BUFF_LEN] = {0};
 uint8 txIndex;
 bool txReadyToSend = false;
+
+morse_t rxMessage[RX_BUFF_LEN] = {0};
+uint8 rxIndex;
+
+uint32 startTime;
+uint32 symbolTime;
+bool symbolInProgress = false;
+bool gapInProgress = false;
+
+void buzzerOn()
+{
+	setDigitalOutput(10,HIGH);
+}
+
+void buzzerOff()
+{
+	setDigitalOutput(10,LOW);
+}
+
+void initRxMessage()
+{
+	//This makes sure we don't receive a message of 0's.
+	rxIndex = RX_BUFF_LEN; 
+}
 
 void initTxMessage()
 {
@@ -37,11 +69,79 @@ void newTxSymbol(morse_t m)
 	txIndex++;
 }
 
+void receiveMessage(uint8 message)
+{
+	uint8 i;
+	rxIndex = 0;
+	for(i = 0; i < RX_BUFF_LEN; i++)
+	{
+		rxMessage[i] = (message >> 2*i) & 0x3;
+	}
+}
+
+void processSymbol(morse_t m)
+{
+	if(m == INVALID) return;
+	
+	startTime = getMs();
+	switch(m)
+	{
+	case DOT:
+		symbolTime = DOT_TIME;
+		buzzerOn();
+	case DASH:
+		symbolTime = DASH_TIME;
+		buzzerOn();
+	case SPACE:
+		symbolTime = SPACE_TIME;
+	}
+	symbolInProgress = true;
+	
+}
+
+
+
 void main()
 {
     systemInit();
     usbInit();
 
 	initTxMessage();
+	initRxMessage();
 	
+	while(1)
+	{
+		if(symbolInProgress &&((getMs() - startTime) > symbolTime))
+		{
+			buzzerOff();
+			symbolInProgress = false;
+			gapInProgress = true;
+			startTime = getMs();
+		}
+		
+		if(gapInProgress && ((getMs() - startTime) > GAP_TIME))
+		{
+			gapInProgress = false;
+		}
+		
+		if(rxIndex < RX_BUFF_LEN && !symbolInProgress && !gapInProgress)
+		{
+			processSymbol(rxMessage[rxIndex]);
+			rxIndex++;
+		}
+
+		
+		//if(radioComRxAvailable() && rxIndex == RX_BUFF_LEN)
+		if(usbComRxAvailable() && rxIndex == RX_BUFF_LEN)
+		{
+			//receiveMessage(radioComRxReceiveByte());
+			receiveMessage(usbComRxReceiveByte());
+			
+			
+		}
+		
+		radioComTxService();
+	}
 }
+
+
